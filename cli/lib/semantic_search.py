9b0b223c556099082ceb506e2fe33e2f6e6a0b4d
Aaro Lankinen <aaro.lankinen@gmail.com>
2026-07-08 15:53:15 +0300

    feat: add embed_text CLI command and implement embedding functionality in SemanticSearch library

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -3,12 +3,34 @@ import torch
 from sentence_transformers import SentenceTransformer, util
 
 class SemanticSearch:
-    def __init__(self, documents: dict[str, str], model: str = "all-MiniLM-L6-v2"):
-        self.documents = documents
+    def __init__(self, model: str = "all-MiniLM-L6-v2"):
         self.model = SentenceTransformer(model)
-        self.document_embeddings = self.model.encode(documents.values(), convert_to_numpy=True)
+
+    def generate_embedding(self, text):
+        return self.model.encode(text, convert_to_numpy=True)
+
+    def search(self, query: str, limit: int = 10) -> list[str]:
+        query_embedding = self.generate_embedding(query)
+        similarities = util.cos_sim(query_embedding, self.document_embeddings)[0]
+        top_k = torch.topk(similarities, k=limit)
+        results = []
+        for score, idx in zip(top_k.values, top_k.indices):
+            results.append(list(self.documents.keys())[idx.item()])
+        return results
+    
 
 def verify_model():
     model = SentenceTransformer('all-MiniLM-L6-v2')
     print(f"Model loaded: {model}")
     print(f"Max sequence length: {model.max_seq_length}")
+
+def embed_text(text):
+    semantic_search = SemanticSearch()
+    embedding = semantic_search.generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
+
+def semantic_search(query: str, limit: int = 10):
+    semantic_search = SemanticSearch()
+    return semantic_search.search(query, limit)
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,17 +1,21 @@
 import argparse
-from lib.semantic_search import verify_model
+from lib.semantic_search import verify_model, embed_text
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     verify_parser = subparsers.add_parser("verify", help="Verify the model")
-
+    embed_parser = subparsers.add_parser("embed_text", help="Embed text")
+    embed_parser.add_argument("text", help="Text to embed")
+    
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+        case "embed_text":
+            embed_text(args.text)
 
 if __name__ == "__main__":
     main()
